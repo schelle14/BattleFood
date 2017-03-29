7eb235eb021239c0c43a9fe0aa2d8441728e4aa1
dpdsw <daniel-paul@gmx.at>
2017-03-29 14:04:29 +0200

    Start_UI Testimp [DD, II]

--- app/src/androidTest/java/at/xp2_2017/battlefood/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/at/xp2_2017/battlefood/ExampleInstrumentedTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
@@ -29,8 +30,15 @@ public class ExampleInstrumentedTest {
     @Test
     public void testClickImage(){
         onView(withId(R.id.IBfoodtop)).perform(click());
+        onView(withId(R.id.button)).perform(click());
         onView(withId(R.id.IBfoodbottom)).perform(click());
     }
+
+   /* @Test
+    public void testImageButton1()
+    {
+
+    }*/
 
 
 
