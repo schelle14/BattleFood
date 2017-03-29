3eb32d83ac604ae665e0193fd188c6b30cf03a90
Bernd Bauernhofer <bernd.bauernhofer@student.tugraz.at>
2017-03-29 14:07:12 +0200

    [BB,AP] Some minimal design changes

--- app/src/androidTest/java/at/xp2_2017/battlefood/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/at/xp2_2017/battlefood/ExampleInstrumentedTest.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -12,6 +14,10 @@ import org.junit.runner.RunWith;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
@@ -40,4 +46,50 @@ public class ExampleInstrumentedTest {
         onView(withText("Register")).perform(click());
 
     }
+
+    @Test
+    public void testRegistrationLabel() throws Exception {
+
+        onView(withText("Register")).perform(click());
+        onView(withId(R.id.textViewRegistration)).check(matches(withText("Registration")));
+    }
+
+
+    @Test
+    public void testUsername() throws Exception {
+
+        onView(withText("Register")).perform((click()));
+        onView(withId(R.id.editTextUserName)).perform(typeText("Max"), closeSoftKeyboard());
+        onView(withId(R.id.editTextUserName)).check(matches(withText("Max")));
+    }
+
+    @Test
+    public void testEmail() throws Exception {
+
+        onView(withText("Register")).perform((click()));
+        onView(withId(R.id.editTextEmail)).perform(typeText("test.email@tugraz.at"), closeSoftKeyboard());
+        onView(withId(R.id.editTextEmail)).check(matches(withText("test.email@tugraz.at")));
+
+    }
+
+    @Test
+    public void testPassword() throws Exception {
+
+        onView(withText("Register")).perform((click()));
+        onView(withId(R.id.editTextPassword)).perform(typeText("abc123"), closeSoftKeyboard());
+        onView(withId(R.id.editTextPassword)).check(matches(withText("abc123")));
+    }
+
+    @Test
+    public void testRetypePassword() throws Exception {
+
+        onView(withText("Register")).perform((click()));
+        onView(withId(R.id.editTextRetypePassword)).perform(typeText("abc123"), closeSoftKeyboard());
+        onView(withId(R.id.editTextRetypePassword)).check(matches(withText("abc123")));
+    }
+
+
+
+
+
 }
