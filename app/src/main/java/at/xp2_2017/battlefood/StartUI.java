5af15844b9a1bed29777456cbaa1133ae24c421a
dpdsw <daniel-paul@gmx.at>
2017-03-29 13:42:39 +0200

    Start_UI Jump between Views [DD, II]

--- app/src/main/java/at/xp2_2017/battlefood/StartUI.java
+++ app/src/main/java/at/xp2_2017/battlefood/StartUI.java
@@ -8,37 +8,59 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 
 public class StartUI extends AppCompatActivity implements View.OnClickListener {
     private ImageButton IBfoodtop;
     private ImageButton IBfoodbottom;
 
-    ImageButton androidImageButton;
+    ImageButton foodImageButton;
+    ImageButton foodImageButton2;
+
+    State start_ui_state;
+    Intent MainActivity;
+    Intent SecondUI;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_ui);
 
-        androidImageButton = (ImageButton) findViewById(R.id.IBfoodtop);
-        androidImageButton.setOnClickListener((View.OnClickListener) this);
+        foodImageButton = (ImageButton) findViewById(R.id.IBfoodtop);
+        foodImageButton.setOnClickListener(this);
+
+        foodImageButton2 = (ImageButton) findViewById(R.id.IBfoodbottom);
+        foodImageButton2.setOnClickListener(this);
 
 
     }
-
+    public enum State{
+        DISHONE, DISHTWO, MENUE
+    }
     @Override
     public void onClick(View v) {
 
-        Intent MainActivity = new Intent(StartUI.this,MainActivity.class);
-        startActivity(MainActivity);
 
-            /* v.setBackgroundResource(R.drawable.food_1);
-            androidImageButton.setBackgroundResource(R.drawable.food_1);*/
+       ImageButton clickedButton = (ImageButton) v;
 
+        switch(clickedButton.getId())
+        {
+            case R.id.IBfoodtop:
+                start_ui_state = State.DISHONE;
+                MainActivity = new Intent(StartUI.this,MainActivity.class);
+                startActivity(MainActivity);
+                break;
+            case R.id.IBfoodbottom:
+                start_ui_state = State.DISHTWO;
+                SecondUI = new Intent(StartUI.this,SecondUI.class);
+                startActivity(SecondUI);
+                break;
+            default:
+                break;
+
+        }
     }
-        /*Bitmap bmp = BitmapFactory.decodeFile("food_1");
-        ImageButton button1 = (ImageButton)findViewById(R.id.IBfoodtop);
-        button1.setImageBitmap(bmp);*/
+
 
 }
