39281d82a07641de142c76cfb9fa5bdd997eaba3
Michael Weiß <michael.weiss@student.tugraz.at>
2017-04-05 11:29:55 +0200

    Login implementation new [mw, dd]

--- app/src/main/java/at/xp2_2017/battlefood/Login.java
+++ app/src/main/java/at/xp2_2017/battlefood/Login.java
@@ -1,15 +1,32 @@
 package at.xp2_2017.battlefood;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
+
+public class Login extends AppCompatActivity implements View.OnClickListener{
 
     private EditText etEmail;
     private EditText etPassword;
     private Button btnLogin;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private TextView txtcheckLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +34,54 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         etEmail = (EditText) findViewById(R.id.etEmailLogin);
+        txtcheckLogin = (TextView) findViewById(R.id.txtcheckLogin);
         etPassword = (EditText) findViewById(R.id.etPasswordLogin);
         btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(this);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                if (firebaseAuth.getCurrentUser() != null) {
+                    //Switch to Mainactivity page
+                    txtcheckLogin.setText("Sucess");
+                    //CHANGE TO REGISTRATION TO START
+                   // startActivity(new Intent(Login.this, RegistrationUI.class));
+
+                }
+            }
+        };
+    }
+
+    private void startSignIn(){
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+        //checks if login is sucessfull
+        if(TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
+            Toast.makeText(Login.this, "Fields are empty", Toast.LENGTH_LONG).show();
+        }
+        else
+        {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(Login.this, "Sign in failed", Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+        protected void onStart() {
+            super.onStart();
+            //Adding auth to mAuthlistener
+            mAuth.addAuthStateListener(mAuthListener);
+        }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
--- app/src/main/java/at/xp2_2017/battlefood/MainActivity.java
+++ app/src/main/java/at/xp2_2017/battlefood/MainActivity.java
@@ -6,11 +6,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import static android.R.attr.onClick;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     public Button buttonReg;
+    public Button buttonLogin;
 
     public void init() {
+        buttonLogin = (Button) findViewById(R.id.btnLoginStart);
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+              Intent login = new Intent(MainActivity.this, Login.class);
+                startActivity(login);
+            }
+        });
         buttonReg = (Button)findViewById(R.id.buttonReg);
         buttonReg.setOnClickListener(new View.OnClickListener() {
             @Override
