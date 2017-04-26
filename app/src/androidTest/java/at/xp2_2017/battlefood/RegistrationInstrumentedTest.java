320432380165763d8d68e629c1eacafac9646c08
Michael Weiß <michael.weiss@student.tugraz.at>
2017-04-26 09:44:31 +0200

    [mw][mh] Merge remote-tracking branch 'origin/feature_Login' into develop

---
+++ app/src/androidTest/java/at/xp2_2017/battlefood/RegistrationInstrumentedTest.java
@@ -0,0 +1,46 @@
+package at.xp2_2017.battlefood;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class RegistrationInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("at.xp2_2017.battlefood", appContext.getPackageName());
+    }
+
+    @Rule
+    public ActivityTestRule<RegistrationUI> mActivityRule = new ActivityTestRule<>(RegistrationUI.class);
+
+
+    @Test
+    public void testRegistrationLabel() throws Exception {
+
+        onView(withText("Register")).perform(click());
+        onView(withId(R.id.textViewRegistration)).check(matches(withText("Registration")));
+    }
+}
