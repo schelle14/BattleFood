26d8c97679418148a267065ecdd8c9ba3cd17505
dpdsw <daniel-paul@gmx.at>
2017-05-10 11:15:39 +0200

    [DD, MH] Watch Votes UI Adapter Changes

--- app/src/main/java/at/xp2_2017/battlefood/VotesUI.java
+++ app/src/main/java/at/xp2_2017/battlefood/VotesUI.java
@@ -1,42 +1,50 @@
 package at.xp2_2017.battlefood;
 
-import android.content.Context;
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.GridView;
+import android.widget.Toast;
 
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
 
 public class VotesUI extends AppCompatActivity{
 
-
-    public Button buttonReg;
-    public Button buttonLog;
-    public Button buttonUpload;
-    public TextView txtMainActivity;
     public Button watchBut;
 
+
+   GridView bgrid;
+    int [] images ={R.drawable.food_1,R.drawable.food_2,R.drawable.food_1,R.drawable.food_2,
+            R.drawable.food_1,R.drawable.food_2,R.drawable.food_1,R.drawable.food_2,
+            R.drawable.food_1,R.drawable.food_2,R.drawable.food_1,R.drawable.food_2};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_votes__ui);
+        setContentView(R.layout.activity_votes_ui);
 
-        BattleAdapter mampe;
+         bgrid =(GridView) findViewById(R.id.gridView2);
+
+        //Adapter
+        BattleAdapter ada = new BattleAdapter(this,images);
+        bgrid.setAdapter(ada);
+
+        //events
+        bgrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Toast.makeText(getApplicationContext(),"Image: "+ position,Toast.LENGTH_SHORT).show();
+
+            }
+        });
 
     }
 
     /*@Override
+
     public void onClick(View v) {
 
     }*/
