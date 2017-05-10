02ae8e15b3d24f8e0badd47f404f5fa673d049c9
dpdsw <daniel-paul@gmx.at>
2017-05-10 09:22:17 +0200

    [DD, MH] Watch Votes UI Adapter

---
+++ app/src/test/java/at/xp2_2017/battlefood/WatchVotesUnitTest.java
@@ -0,0 +1,19 @@
+package at.xp2_2017.battlefood;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class WatchVotesUnitTest {
+
+    @Test
+    public void testLogin() throws Exception {
+
+
+    }
+}
