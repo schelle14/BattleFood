1617b6ad94322083712b29c6e3b51399c25f5d4f
Bernd Bauernhofer <bernd.bauernhofer@student.tugraz.at>
2017-05-17 10:40:11 +0200

    [FH,BB] Reading recipes from database, display them in imagebutton

--- app/src/main/java/at/xp2_2017/battlefood/StartUI.java
+++ app/src/main/java/at/xp2_2017/battlefood/StartUI.java
@@ -1,6 +1,8 @@
 package at.xp2_2017.battlefood;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -8,7 +10,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 
-import com.google.firebase.database.ChildEventListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,10 +21,14 @@ import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 public class StartUI extends AppCompatActivity implements View.OnClickListener {
-    private DatabaseReference mDatabase;
+    public DatabaseReference mDatabase;
+    public StorageReference mStorage;
     public ImageButton foodImageButtonTop;
     public ImageButton foodImageButtonBot;
     public Button menuButton;
@@ -73,13 +81,74 @@ public class StartUI extends AppCompatActivity implements View.OnClickListener {
         setContentView(R.layout.activity_start_ui);
 
         //Load random recipes
-        //mStorageRef = FirebaseStorage.getInstance().getReference();
-        //StorageReference pathReference = mStorageRef.child("images/testpicture1.jpg");
-
         mDatabase = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference recipes = mDatabase.child("Recipes");
+        mStorage = FirebaseStorage.getInstance().getReference();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        mDatabase.child("Users/"+user.getUid()+"/RecipeKey").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                final List<String> recipes = Arrays.asList(dataSnapshot.getValue().toString().split(";"));
+
+                mDatabase.child("Recipe").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        List<String> unvoted_recipes = new ArrayList<>();
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            if (!recipes.contains(snapshot.getKey())) {
+                                unvoted_recipes.add(snapshot.getKey());
+                            }
+                        }
+                        Log.d("das ist ein test", recipes.toString());
+                        Log.d("das ist ein test22222", unvoted_recipes.toString());
+
+                        Random rand = new Random();
+                        int n_1;
+                        int n_2;
+                        do {
+                            n_1 = rand.nextInt(((int)dataSnapshot.getChildrenCount()));
+                            n_2 = rand.nextInt((int)dataSnapshot.getChildrenCount());
+                        } while (n_1 != n_2);
 
 
+                        StorageReference image_reference_1 = mStorage.child("images/" + unvoted_recipes.get(n_1));
+                        StorageReference image_reference_2 = mStorage.child("images/" + unvoted_recipes.get(n_2));
+
+                        final long TEN_MEGABYTE = 1024 * 1024 * 10;
+                        image_reference_1.getBytes(TEN_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                            @Override
+                            public void onSuccess(byte[] bytes) {
+
+                                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+
+                                foodImageButtonTop.setImageBitmap(Bitmap.createScaledBitmap(bmp, foodImageButtonTop.getWidth(),
+                                        foodImageButtonTop.getHeight(), false));
+                            }
+                        });
+                        image_reference_2.getBytes(TEN_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                            @Override
+                            public void onSuccess(byte[] bytes) {
+                                Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+
+                                foodImageButtonBot.setImageBitmap(Bitmap.createScaledBitmap(bmp, foodImageButtonBot.getWidth(),
+                                        foodImageButtonBot.getHeight(), false));
+
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
 
         init();
     }
