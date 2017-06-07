31e3ee4206264f7a099a420d41dc28c267b12efd
dpdsw <daniel-paul@gmx.at>
2017-06-07 14:02:11 +0200

    [DD, AP] UI fixes, unnecessary button deleted

--- app/src/main/java/at/xp2_2017/battlefood/MainActivity.java
+++ app/src/main/java/at/xp2_2017/battlefood/MainActivity.java
@@ -12,8 +12,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public Button buttonReg;
     public Button buttonLog;
     public TextView txtMainActivity;
-    public Button buttonWatch;
-
+    
     public void init() {
         buttonReg = (Button)findViewById(R.id.buttonReg);
         buttonReg.setOnClickListener(new View.OnClickListener() {
